4df23982cd10ecf9a87628c0965374d66280f5f3
sanzharsp <sanzharsapar1@gmail.com>
2022-09-14 12:32:07 +0600

    Auth and JWT tokens correcting base url

--- src/components/backend-server-url.js
+++ src/components/backend-server-url.js
@@ -1,8 +1,8 @@
 /*хранилище url Django backend */
     const url={
 
-        //"baseUrl":"https://kaznews.pythonanywhere.com",/* базовый url */
-        "baseUrl":"http://127.0.0.1:8000", // тестовый url
+        "baseUrl":"https://kaznews.pythonanywhere.com",/* базовый url */
+        //"baseUrl":"http://127.0.0.1:8000", // тестовый url
         "url_get_data":"/api/v1/news/get/content/",/*загрузка по id */
         "lates_news":"/api/v1/news/get/last_news?page=",/*Последние новости*/
         "main_news":"/api/v1/news/get/main_news?page=",
