796effc53df610a5482fb9bbbfa6af971392b21d
sanzharsp <sanzharsapar1@gmail.com>
2022-10-31 19:22:35 +0600

    design fix bugs

--- src/components/LinkToPost/LinkToPost.jsx
+++ src/components/LinkToPost/LinkToPost.jsx
@@ -5,7 +5,12 @@ import './LinkToPost.css';
 import Slideshow from '../img/ImageSlider'
 import url from '../backend-server-url'
 import { useParams } from "react-router-dom";
+import Container from '@mui/material/Container';
 import Footer from '../footer/footer';
+import CardContent from '@mui/material/CardContent';
+import Card from '@mui/material/Card';
+
+
 
 let baseurl = url.baseUrl
 let url_get_data=url.url_get_data
@@ -33,36 +38,36 @@ useEffect(()=>{
 
 
     return (
-      
-      <div className="content">
+      <>
+      <Container>
       { post.map(posts =>
         <div key={posts.id}>
-        <div className="content-title" dangerouslySetInnerHTML={{ __html: posts.title }} />
-        
-        <Slideshow baseurl={baseurl} image1={`${baseurl}${posts.image1}`} image2={`${baseurl}${posts.image2}`} image3={`${baseurl}${posts.image3}`} />
-      
-
-           <div className="post">
-         
-              <div className="post-content">
-       
-        
-                <div dangerouslySetInnerHTML={{ __html: posts.content_text }}/>
-                <span className="post-date"><i className="fa fa-clock-o"></i>{posts.published_date}</span>
-       
-            
-              </div>
-              
-            </div>
-        
-   
-        <Footer/>
+        <div className="content-title content" dangerouslySetInnerHTML={{ __html: posts.title }} />
+        <div className="content">
+        <Slideshow  baseurl={baseurl} image1={`${baseurl}${posts.image1}`} image2={`${baseurl}${posts.image2}`} image3={`${baseurl}${posts.image3}`} />
         </div>
+              
+          <Card variant="outlined">
+         
+          <Container>
+       
+        <CardContent>
+                <div dangerouslySetInnerHTML={{ __html: posts.content_text }}/>
+                </CardContent>
+                </Container>
+          
+              
+                </Card>
+         
+                </div>
+       
         
     )}
-       
-
-      </div>
+   
+ 
+    </Container>
+    <Footer/>
+    </>
         
       
     )
